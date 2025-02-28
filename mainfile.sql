ee0bd07ace3b8d3c2f93b7423865fa9d94b037b8
Lee1049 <leebusuttil@gmail.com>
2025-02-28 16:23:20 +0000

    queries to the database

---
+++ mainfile.sql
@@ -0,0 +1,14 @@
+\c nc_news
+
+-- Making some queries to the database
+
+-- SELECT * FROM comments;
+-- SELECT * FROM users;
+-- SELECT username FROM users;
+-- SELECT * FROM articles WHERE topic = 'coding';
+-- SELECT * FROM comments WHERE votes <0;
+-- SELECT * FROM topics;
+-- SELECT * FROM articles WHERE author = 'grumpy19';
+-- SELECT * FROM comments WHERE votes >10;
+-- SELECT article_id FROM articles;
+-- SELECT article_id FROM comments;
