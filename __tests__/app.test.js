3982c79476db3725a92721dcd040d9d917fddae8
Lee1049 <leebusuttil@gmail.com>
2025-03-10 16:46:33 +0000

    3 complete. further testing for error handling carried out

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,7 @@ const seed = require("../db/seeds/seed");
 const data = require("../db/data/test-data");
 const app = require("../app");
 const request = require("supertest");
+const articles = require("../db/data/test-data/articles");
 
 beforeEach(() => {
   return seed(data);
@@ -19,7 +20,6 @@ describe("GET /api", () => {
       .get("/api")
       .expect(200)
       .then(({ body: { endpoints } }) => {
-        // console.log("response", endpoints);
         expect(endpoints).toEqual(endpointsJson);
       });
   });
@@ -32,10 +32,48 @@ describe("GET /api/topics", () => {
       .expect(200)
       .then(({ body: { topics } }) => {
         expect(Array.isArray(topics)).toBe(true);
+        expect(topics.length).toBeGreaterThan(0);
         topics.forEach((topic) => {
           expect(topic).toHaveProperty("slug");
           expect(topic).toHaveProperty("description");
         });
+      });
+  });
+});
+
+describe("GET /api/articles/:article_id", () => {
+  test("200: responds with a single article", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({ body: { article } }) => {
+        expect(article).toHaveProperty("article_id", 1);
+        expect(article).toHaveProperty("title");
+        expect(article).toHaveProperty("author");
+        expect(article).toHaveProperty("body");
+        expect(article).toHaveProperty("topic");
+        expect(article).toHaveProperty("created_at");
+        expect(article).toHaveProperty("votes");
+        expect(article).toHaveProperty("article_img_url");
+      });
+  });
+  test("404: responding with an error if there is no article ", () => {
+    return request(app)
+      .get("/api/articles/999999")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Unable to find the article");
+      });
+  });
+});
+
+describe("Error handling", () => {
+  test("500: responds with an internal server error ", () => {
+    return request(app)
+      .get("/api/errorpathway")
+      .expect(500)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Internal Server Error");
       });
   });
 });
